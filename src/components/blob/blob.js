29c5e59c9bc85e5e20ff3468e77fafae565ba428
karan singh <karansinghk0@gmail.com>
2021-05-28 11:29:57 +0530

    added card tilt

--- src/components/blob/blob.js
+++ src/components/blob/blob.js
@@ -28,22 +28,36 @@ const Blob = () => {
     //     }, 2000);
     //   }}
     // ></div>
-    <div
-      style={{
-        width: "100%",
-        height: "100%",
-        position: "absolute",
-        top: "0%",
-        left: "0%",
-        zIndex: -1,
-        overflow: "hidden",
-        // background: "yellow",
-        backgroundImage:
-          "url('https://css-tricks.com/wp-content/uploads/2019/02/blob.gif')",
-        backgroundRepeat: "no-repeat",
-        backgroundPosition: "100% 50%",
-      }}
-    ></div>
+    <span className="blob">
+      <svg
+        viewBox="0 0 200 200"
+        height="2em"
+        width="2em"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fill="#F2A365"
+          d="M43.7,-68.1C54.1,-61.4,58.1,-44.9,64.6,-29.8C71,-14.7,79.9,-1,78.7,11.7C77.5,24.4,66.3,36,56,49.3C45.7,62.6,36.5,77.6,23.5,82.6C10.6,87.5,-6.1,82.4,-21.5,76.4C-36.9,70.4,-51.1,63.5,-58.3,52.2C-65.4,40.9,-65.5,25.2,-65.7,10.7C-65.8,-3.8,-66.1,-17.1,-63.7,-31.9C-61.3,-46.7,-56.2,-63.1,-45.1,-69.5C-34,-75.8,-17,-72.2,-0.2,-72C16.7,-71.7,33.4,-74.8,43.7,-68.1Z"
+          transform="translate(100 100)"
+        />
+      </svg>
+    </span>
+    // <div
+    //   style={{
+    //     width: "100%",
+    //     height: "100%",
+    //     position: "absolute",
+    //     top: "0%",
+    //     left: "0%",
+    //     zIndex: -1,
+    //     overflow: "hidden",
+    //     // background: "yellow",
+    //     backgroundImage:
+    //       "url('https://css-tricks.com/wp-content/uploads/2019/02/blob.gif')",
+    //     backgroundRepeat: "no-repeat",
+    //     backgroundPosition: "100% 50%",
+    //   }}
+    // ></div>
   );
 };
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef } from "react";
+import { Helmet } from "react-helmet";
 import { graphql } from "gatsby";
 import { StaticImage, GatsbyImage } from "gatsby-plugin-image";
 import "../global/global.css";
@@ -8,6 +9,8 @@ import {
   heroSection,
   heroSectionTitle,
   contentContainer,
+  tOver,
+  heroSectionWrapper,
 } from "../pageStyles/index.module.css";
 const offsetX = 25;
 const offsetY = 25;
@@ -31,6 +34,13 @@ const Home = ({ data }) => {
 
   return (
     <>
+      <Helmet>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,300;0,400;0,700;1,300;1,400&display=swap"
+          rel="stylesheet"
+        />
+      </Helmet>
       <div
         style={{
           left: 0,
@@ -42,17 +52,34 @@ const Home = ({ data }) => {
         className="cursor2"
       ></div>
       <Header />
-      <div className={heroSection}>
-        <div className={contentContainer}>
-          <h1 className={heroSectionTitle}>I am Karandeep Singh</h1>
-        </div>
-        {/* <div
+      <div className={heroSectionWrapper}>
+        <span className={tOver}></span>
+        <span className={tOver}></span>
+        <span className={tOver}></span>
+        <span className={tOver}></span>
+        <span className={tOver}></span>
+        <span className={tOver}></span>
+        <span className={tOver}></span>
+        <span className={tOver}></span>
+        <span className={tOver}></span>
+
+        <div className={heroSection}>
+          <div className={contentContainer}>
+            <h1 className={heroSectionTitle}>
+              Hi,
+              <br />I <br />
+              am
+              <br /> Karandeep Singh
+            </h1>
+          </div>
+          {/* <div
         style={{ left: 0, right: 0 }}
         ref={cursorRef}
         className="cursor-wrapper"
       >
         <div className="cursor"></div>
       </div> */}
+        </div>
       </div>
       <Blob />
     </>

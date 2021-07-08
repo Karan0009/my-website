39eb2e0f71474ec1360bcd4c1230e493f080d2d1
karan singh <karansinghk0@gmail.com>
2021-07-08 23:03:08 +0530

    new template,changed gatsby config, changed gatsbynode file

---
+++ src/templates/blog.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { Link } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
+
+import Layout from "../components/layout";
+import Seo from "../components/seo";
+
+const Blog = () => (
+  <Layout>
+    <Seo title="blog" />
+  </Layout>
+);
+
+export default Blog;

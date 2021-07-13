100019c0ff79ba2b5b4925da2afd9313724d0f61
karan singh <karansinghk0@gmail.com>
2021-07-13 20:24:25 +0530

    blog pages changed

--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 // import { Link } from "gatsby";
 // import { StaticImage } from "gatsby-plugin-image";
 import { graphql, useStaticQuery } from "gatsby";
@@ -29,25 +29,51 @@ const BlogPage = ({ data }) => {
   //   `
   // );
   //   console.log(allMarkdownRemark.edges[0].node.frontmatter);
+  const queryData = useStaticQuery(graphql`
+    query asdsadf {
+      allMarkdownRemark(
+        sort: { order: DESC, fields: frontmatter___publishedDate }
+      ) {
+        edges {
+          node {
+            frontmatter {
+              title
+              isActive
+              publishedDate(fromNow: true)
+              featuredImage
+            }
+            html
+          }
+        }
+      }
+    }
+  `);
+  console.log("this is query data", queryData.allMarkdownRemark.edges);
   return (
     <>
-      <Layout>
-        {/* <Seo title="blogs" /> */}
-        <section className="blogs-grid">
-          {/* {allMarkdownRemark.edges.map((d) => (
+      {/* <Layout> */}
+      {/* <Seo title="blogs" /> */}
+      {/* <section className="blogs-grid"> */}
+      <div
+        style={{ color: "white" }}
+        dangerouslySetInnerHTML={{
+          __html: `${queryData.allMarkdownRemark.edges[0].node.html}`,
+        }}
+      />
+      {/* {allMarkdownRemark.edges.map((d) => (
             <article className="blog-card">
               <header className="blog-card-title">
                 {d.node.frontmatter.title}
               </header>
             </article>
           ))} */}
-        </section>
+      {/* </section> */}
 
-        {/* <p style={{ color: "white" }}>{site.siteMetadata.title}</p> */}
-        {/* {data.MyQuery.allMarkdownRemark.edges.map((d) => (
+      {/* <p style={{ color: "white" }}>{site.siteMetadata.title}</p> */}
+      {/* {data.MyQuery.allMarkdownRemark.edges.map((d) => (
       <p style={{ color: "white" }}>{d.node.frontmatter.title}</p>
     ))} */}
-      </Layout>
+      {/* </Layout> */}
     </>
   );
 };
--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,14 +1,36 @@
 import * as React from "react";
-import { Link } from "gatsby";
+import { Link, useStaticQuery, graphql } from "gatsby";
 import { StaticImage } from "gatsby-plugin-image";
 
 import Layout from "../components/layout";
 import Seo from "../components/seo";
 
-const Blog = () => (
-  <Layout>
-    <Seo title="blog" />
-  </Layout>
-);
+const Blog = () => {
+  // const data = useStaticQuery(graphql`
+  //   query MyQuery {
+  //     allMarkdownRemark(
+  //       sort: { order: DESC, fields: frontmatter___publishedDate }
+  //     ) {
+  //       edges {
+  //         node {
+  //           frontmatter {
+  //             title
+  //             isActive
+  //             publishedDate(fromNow: true)
+  //             featuredImage
+  //           }
+  //           html
+  //         }
+  //       }
+  //     }
+  //   }
+  // `);
+  // console.log(data);
+  return (
+    <Layout>
+      <Seo title="blog" />
+    </Layout>
+  );
+};
 
 export default Blog;
